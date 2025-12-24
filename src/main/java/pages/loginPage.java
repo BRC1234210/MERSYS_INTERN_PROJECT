44b5bde1cb92af3a3caadbb4d94a803fce5bf2bb
BRC1234210 <askbarrcan41@gmail.com>
2025-12-24 19:52:41 +0300

    created

---
+++ src/main/java/pages/loginPage.java
@@ -0,0 +1,17 @@
+package pages;
+
+import io.qameta.allure.internal.shadowed.jackson.databind.ser.Serializers;
+import org.openqa.selenium.WebDriver;
+
+public class loginPage extends BasePage {
+
+
+
+
+
+
+
+    public loginPage(WebDriver driver) {
+        super(driver);
+    }
+}
