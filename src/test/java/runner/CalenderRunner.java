acf91989d7542a7b22061ad15b2e345d90090a65
nldn24 <ahmetnaldan24@hotmail.com>
2026-01-16 15:56:23 +0300

    Us23-24-25(CalenderFeature) created

---
+++ src/test/java/runner/CalenderRunner.java
@@ -0,0 +1,13 @@
+package runner;
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+@CucumberOptions(
+        features = "src/test/resources/features/calendar.feature",
+        glue = {"StepDefinitions", "Hooks"},
+        plugin = {"pretty", "html:target/cucumber-report.html"}
+)
+
+public class CalenderRunner extends AbstractTestNGCucumberTests {
+}
